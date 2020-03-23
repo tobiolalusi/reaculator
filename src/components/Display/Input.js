24d165243921838370d2526003269054ee6cd36b
Oluwatobiloba Olalusi <o.olalusi@esar-gmbh.de>
2020-03-23 16:34:32 +0100

    turned off autocomplete

--- src/components/Display/Input.js
+++ src/components/Display/Input.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Input = ({ onSubmit, value, onChange }) =>
 	<form className='col-12 px-0 pt-2 pb-0' onSubmit={ onSubmit }>
-		<input id='display-input' maxLength='99' className='DisplayInput form-control col-12 pb-0 bg-light text-dark' value={ value } onChange={ onChange } />
+		<input id='display-input' maxLength='99' autoComplete="off" className='DisplayInput form-control col-12 pb-0 bg-light text-dark' value={ value } onChange={ onChange } />
 	</form>
 
 export { Input };
